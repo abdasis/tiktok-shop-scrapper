5ea09816f5270d17b09a749dd56ae1cfed98315f
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2024-07-15 15:32:37 +0700

    tambah webhook untuk auto replay

--- app/Http/Controllers/TIktokController.php
+++ app/Http/Controllers/TIktokController.php
@@ -75,41 +75,42 @@ class TIktokController extends Controller
 		 $response = Telegram::bot('mybot');
 		 $response_collection = $response->getWebhookUpdate();
 		 $chat_colleciton = collect($response_collection->getMessage())->sortByDesc('date');
+		 Log::info($chat_colleciton);
 		 $pesan = $chat_colleciton['text'];
-		 $username = $chat_colleciton['from']['username'];
+		 $chat_id = $chat_colleciton['chat']['id'];
+		 $username = $chat_colleciton['reply_to_message']['from']['username'];
+		 $first_name = $chat_colleciton['from']['first_name'];
+		 $last_name = $chat_colleciton['from']['last_name'];
+		 $full_name = $first_name . ' ' . $last_name;
 		 if (isset($chat_colleciton['message_thread_id'])) {
 			$tread_id = $chat_colleciton['message_thread_id'];
-			if ($username === 'ttaffl' && $tread_id != 109) {
+			if (strpos($pesan, "https://vt.tokopedia.com") === false && strpos($pesan, "https://shop-id.tokopedia.com") === false) {
 			   $response->sendMessage([
-				  'chat_id' => "-1002243059927",
+				  'chat_id' => $chat_id,
 				  'message_thread_id' => $tread_id,
-				  'text' => "Maaf kak @{$username}, untuk melakukan request gambar produk, kirim linknya di topik <b>Request Produk</b> Ya, Terima kasih sudah mengikuti aturan grup ini"
-			   ]);
-			   return false;
-			}
-			if (strpos($pesan, "https://vt.tokopedia.com") !== false && strpos($pesan, "https://shop-id.tokopedia.com") !== false) {
-			   $response->sendMessage([
-				  'chat_id' => "-1002243059927",
-				  'message_thread_id' => 3,
-				  'text' => 'Maaf kak @'.$username.', link produk yang kamu kirimkan tidak sesuai dengan ketentuan. Silahkan kirimkan pesan yang sesuai. contoh https://vt.tokopedia.com/t/ZSYsLAQ6S/',
+				  'text' => 'Maaf kak '.$first_name.', link produk yang kamu kirimkan tidak sesuai dengan ketentuan. Silahkan kirimkan link product yang sesuai. contoh https://vt.tokopedia.com/t/ZSYsLAQ6S/',
 				  'parse_mode' => 'HTML'
 			   ]);
 			   return false;
 			} else {
-			   
+			   if ($tread_id !== 109) {
+				  $response->sendMessage([
+					 'chat_id' => $chat_id,
+					 'message_thread_id' => $tread_id,
+					 'text' => 'Maaf kak '.$first_name.', kamu mengirim pesan product di topik yang salah, silahkan kirim ulang di topik <b>Request Product </b>',
+				  ]);
+			   }
 			   $response->sendMessage([
-				  'chat_id' => "-1002243059927",
+				  'chat_id' => $chat_id,
 				  'message_thread_id' => 109,
-				  'text' => "Halo kak @$username product yang kamu kirimkan sedang saya proses, silahkan tunggu dan sambil pantau topik Produk Terlaris ya 🥰",
+				  'text' => "Halo kak $first_name product yang kamu kirimkan sedang saya proses, silahkan tunggu dan sambil pantau topik <b>Produk Terlaris</b> ya 🥰",
 				  'parse_mode' => 'HTML'
 			   ]);
 			   $url = $pesan;
 			   $response = Http::post('http://localhost:3333/tikthop-scrapper', [
 				  'url' => $url
 			   ])->collect();
-			   
 			   $image_links = $response['data']['imgLinks'];
-			   
 			   $media = [];
 			   foreach ($image_links as $image_link) {
 				  $type = [
@@ -119,9 +120,8 @@ class TIktokController extends Controller
 				  ];
 				  array_push($media, $type);
 			   }
-			   
 			   //caption title dan link link untuk telegram
-			   $caption = "<b>{$response['data']['title']}</b>\n\nLink Produk: <a href='{$response['data']['url']}'>{$response['data']['url']}</a>👈\n\nDibuat Oleh: Kak @{$username} 🥰";
+			   $caption = "<b>{$response['data']['title']}</b>\n\nLink Produk: <a href='{$response['data']['url']}'>{$response['data']['url']}</a>👈\n\nDibuat oleh: Kak $full_name 🥰";
 			   $media[0]['caption'] = $caption;
 			   
 			   $response = Telegram::bot('mybot');
@@ -130,15 +130,18 @@ class TIktokController extends Controller
 				  'message_thread_id' => 3,
 				  'media' => json_encode($media),
 			   ]);
+			   return true;
 			}
 		 }
 		 else{
 			Log::info('Diluar forums');
+			return true;
 		 }
 		 return true;
 		 
 	  } catch (Exception $exception) {
 		 report($exception->getMessage());
+		 return true;
 	  } finally {
 		 return true;
 	  }
